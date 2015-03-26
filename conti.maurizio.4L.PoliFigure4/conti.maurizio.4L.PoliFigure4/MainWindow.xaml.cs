f5bdf6ba67103df6faa0bf86f79d983b94133da6
posta@maurizioconti.com <posta@maurizioconti.com>
2015-03-26 17:08:04 +0100

    Aggiunto PoliFigure4
    PoliFigure4 legge un file CSV, lo converte in una gerarchia di classi
    Uqadrato, Rettangolo, Triangolo che derivano da Figura e visualizza
    tutto in una ListView

---
+++ conti.maurizio.4L.PoliFigure4/conti.maurizio.4L.PoliFigure4/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using conti.maurizio._4L.PoliFigure4.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace conti.maurizio._4L.PoliFigure4
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Metto tutto sotto try catch.
+            // Il file potrebbe non esistere
+            try
+            {
+                // Tutte le ficure che carico da disco le metto qui
+                List<Figura> figure = new List<Figura>();
+                
+                // Apro il file e scarto la prima riga
+                StreamReader fileIn = new StreamReader("Figure.csv");
+                fileIn.ReadLine();
+
+                // Leggo tutte le righe, una alla volta.
+                do
+                {
+                    // Il procedimento è questo: 
+                    // Leggo la riga e con Split() la divido nelle sue parti
+                    string riga = fileIn.ReadLine();
+                    string[] campi = riga.Split(';');
+
+                    // Guardo al primo campo e decido quale oggetto creare.
+                    // Poi lo creo e lo aggiungo al contenitore delle figure.
+                    if (campi[0] == "Quadrato")
+                        figure.Add(new Quadrato(riga));
+                    else if (campi[0] == "Rettangolo")
+                        figure.Add(new Rettangolo(riga));
+                    else if (campi[0] == "Triangolo")
+                        figure.Add(new Triangolo(riga));
+
+                    // Nota: Ogni oggetto è dotato di un costruttore particolare 
+                    // che gli consente di crearsi dalla String che gli viene passata
+                }
+                while (!fileIn.EndOfStream); // Fino alla fine del file
+
+                // Le figure lette vengono visualizzate su un componente ListView
+                lvFigure.ItemsSource = figure;
+            }
+            catch (Exception err) { MessageBox.Show(err.Message); }
+        }
+    }
+}
---
+++ conti.maurizio.4L.PoliFigure4/conti.maurizio.4L.PoliFigure4/Models/Figura.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conti.maurizio._4L.PoliFigure4.Models
+{
+    public class Figura
+    {
+        public double Misura1 { get; set; }
+        public double Misura2 { get; set; }
+        public double Misura3 { get; set; }
+        
+        public Figura() 
+        {
+            Misura1 = 0;
+            Misura2 = 0;
+            Misura3 = 0;
+        }
+        
+        public Figura(string riga)
+        {
+            string[] campi = riga.Split(';');
+            Misura1 = Convert.ToDouble(campi[1]);
+            Misura2 = Convert.ToDouble(campi[2]);
+            Misura3 = Convert.ToDouble(campi[3]);
+        }
+        public override string ToString() { return "Misura1: " + Misura1 + ", Misura2: " + Misura2 + " Misura3: " + Misura3; }
+
+    }
+
+    public class Quadrato : Figura
+    {
+        public double Lato { get { return Misura1; } }
+        
+        public Quadrato(string riga) : base(riga) { }
+        public override string ToString() { return "Quadrato. Lato: " + Lato; }
+    }
+    public class Rettangolo : Figura
+    {
+        public double Base { get { return Misura1; } }
+        public double Altezza { get { return Misura2; } }
+
+        public Rettangolo(string riga) : base(riga) { }
+        public override string ToString() { return "Rettangolo. Base:" + Base + ", Altezza:" + Altezza; }
+    }
+
+    public class Triangolo : Figura
+    {
+        public double Lato1 { get { return Misura1; } }
+        public double Lato2 { get { return Misura2; } }
+        public double Lato3 { get { return Misura3; } }
+
+        public Triangolo(string riga) : base(riga) { }
+        public override string ToString() { return "Triangolo. Lato1:" + Lato1 + ", Lato2:" + Lato2 + "  Lato3:" + Lato3; }
+    }
+
+}
