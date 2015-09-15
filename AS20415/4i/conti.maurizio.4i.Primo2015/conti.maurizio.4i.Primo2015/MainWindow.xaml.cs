44ae884829f3a85e3642e0c1978df5cf16058b80
Maurizio Conti <mconti65@gmail.com>
2015-09-15 09:41:20 +0200

    Agiunto programma Primo2015 per 4i

---
+++ AS20415/4i/conti.maurizio.4i.Primo2015/conti.maurizio.4i.Primo2015/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace conti.maurizio._4i.Primo2015
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Hello World!!!");
+
+            List<string> elenco = new List<string>();
+            elenco.Add("Maurizio");
+            elenco.Add("Marco");
+
+            lstElenco.ItemsSource = elenco;
+
+        }
+    }
+}
