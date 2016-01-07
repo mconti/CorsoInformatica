be9da045db5a5d654a1f69523af5d6898316961d
Maurizio Conti <mconti65@gmail.com>
2016-01-07 17:43:52 +0100

    Aggiunto Tutorial XML

---
+++ Desktop/conti.maurizio.XMLTutorial1/conti.maurizio.XMLTutorial1/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace conti.maurizio.XMLTutorial1
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApriClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                XElement dati = XElement.Load("Dati.xml");
+
+                // Tutti gli elementi contentuti in root
+                // Che siano "Libri" oppure no
+                IEnumerable<XElement> tuttiGliElementi = dati.Elements();
+
+                // Tutti gli elementi "Libri" contentuti in root
+                // Solo gli elementi "Libri" (nel file Dati.xml ne abbiamo uno ma è comunque un vettore)
+                IEnumerable<XElement> tuttiIlibri = dati.Elements("Libri");
+
+                // Il primo elemento "Libri" contenuto in root
+                XElement libri = dati.Elements("Libri").First();
+
+                // Tutti i "Libro" contenuti in libri
+                IEnumerable<XElement> tuttiIlibro = dati.Elements("Libri").First().Elements("Libro");
+
+                // Il primo elemento "Libro" contenuto in libri
+                XElement primoLibro = dati.Elements("Libri").First().Elements("Libro").First();
+
+                // Stampa tutto l'elemento (<Libro Titolo="Harry Potter" Autore="J. K. Rowling">FotoHP.jpg</Libro>)
+                Console.WriteLine(primoLibro);
+
+                // Stampa tutto l'attributo (Titolo="Harry Potter")
+                Console.WriteLine(primoLibro.Attribute("Titolo"));
+
+                // Stampa solo il valore (Harry Potter)
+                Console.WriteLine(primoLibro.Attribute("Titolo").Value);
+
+                // Stampa il valore contenuto tra il tag di apertura e il tag di chiusura (FotoHP.jpg)
+                Console.WriteLine(primoLibro.Value);
+
+                dgDati.ItemsSource = from l in dati.Elements("Libri").First().Elements("Libro")
+                                     select new Libro(l);
+
+            }
+            catch (Exception Errore)
+            {
+                MessageBox.Show(Errore.Message);
+            }
+        }
+    }
+}
---
+++ Desktop/conti.maurizio.XMLTutorial1/conti.maurizio.XMLTutorial1/Models/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace conti.maurizio.XMLTutorial1
+{
+    class Libro
+    {
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+
+        public Libro(XElement e)
+        {
+            Titolo = e.Attribute("Titolo").Value;
+            Autore = e.Attribute("Autore").Value;
+        }
+    }
+}
