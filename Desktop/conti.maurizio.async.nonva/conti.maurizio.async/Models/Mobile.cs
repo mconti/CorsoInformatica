392d911c150c41965a7b4658c6911e401da091bd
Maurizio Conti <mconti65@gmail.com>
2016-01-07 18:55:27 +0100

    Questo Async non va! Come mai?

---
+++ Desktop/conti.maurizio.async.nonva/conti.maurizio.async/Models/Mobile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conti.maurizio.async
+{
+    public class Mobile
+    {
+        public string Marca { get; set; }
+        public string Modello { get; set; }
+        public string Foto { get; set; }
+        public int Prezzo { get; set; }
+        public string Tipo { get; set; }
+
+        public Mobile() { }
+
+        public Mobile(string riga)
+        {
+            string[] campi = riga.Split(';');
+
+            Marca = campi[0];
+            Modello = campi[1];
+            Foto = campi[2];
+            Prezzo = Convert.ToInt32(campi[3]);
+            Tipo = campi[4];
+        }
+
+    }
+}
---
+++ Desktop/conti.maurizio.async.nonva/conti.maurizio.async/Models/Mobili.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conti.maurizio.async
+{
+    public class Mobili : ObservableCollection<Mobile>
+    {
+        public Mobili()
+        { }
+
+        public Mobili(string NomeFile)
+        {
+            StreamReader sr = new StreamReader(NomeFile);
+            sr.ReadLine();
+			
+            while (!sr.EndOfStream)
+            {
+                Add(new Mobile(sr.ReadLine()));
+            }
+        }
+
+    }
+}
