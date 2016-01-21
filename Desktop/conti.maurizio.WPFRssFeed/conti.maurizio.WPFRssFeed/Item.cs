d567f2eb3309f8dd1856e64158dca964f0c4a377
Maurizio Conti <mconti65@gmail.com>
2016-01-21 18:30:21 +0100

    Aggiunto Client RSS WPF

---
+++ Desktop/conti.maurizio.WPFRssFeed/conti.maurizio.WPFRssFeed/Item.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Xml.Linq;
+
+namespace conti.maurizio.WPFRssFeed
+{
+    public class Item
+    {
+        public static int id { get; set; }
+        public string Titolo { get; set; }
+        public BitmapImage Immagine { get; set; }
+
+        public Item(XElement item)
+        {
+            id++;
+
+            try { Titolo = item.Element("title").Value; } catch { }
+
+            try
+            {
+                string urlImmagine = item.Element("enclosure").Attribute("url").Value;
+                Immagine = new BitmapImage(new Uri(urlImmagine));
+            }
+            catch { }
+        }
+    }
+
+    public class Items : List<Item>
+    {
+        public int id { get; set; }
+        public string Url { get; set; }
+
+        public Items( string url )
+        {
+            Url = url;
+            XElement e = XElement.Load(Url);
+
+            foreach( XElement elemento in e.Element("channel").Elements("item") )
+            {
+                Add(new Item(elemento));
+            }
+        }
+    }
+
+    public class Sorgente
+    {
+        public int id { get; set; }
+        public string Url { get; set; }
+        public Items Items { get; set; }
+
+        public Sorgente() { }
+        public Sorgente(string url)
+        {
+            Url = url;
+            Items = new Items( url );
+        }
+
+    }
+}
---
+++ Desktop/conti.maurizio.WPFRssFeed/conti.maurizio.WPFRssFeed/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace conti.maurizio.WPFRssFeed
+{
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.IsEnabled = false;
+            btnUpdate_Click(sender, null);
+            timer.IsEnabled = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(2000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            //string url = "https://news.google.it/?output=rss";
+            string url = "http://www.ilmattino.it/rss/home.xml";
+
+            btnUpdate.IsEnabled = false;
+            txtUpdate.Text = "updating...";
+
+            Sorgente s = new Sorgente(url);
+            lvItems.ItemsSource = s.Items;
+
+            txtUpdate.Text = url + " - " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString();
+            btnUpdate.IsEnabled = true;
+        }
+    }
+}
+
