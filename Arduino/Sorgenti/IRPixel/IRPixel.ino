e64034a918d71c1bd23dbb498f7751a522a1e4c1
Maurizio Conti <mconti65@gmail.com>
2016-05-24 16:56:48 +0200

    Modifica fatta da Giulia e Valentina

--- Arduino/Sorgenti/IRPixel/IRPixel.ino
+++ Arduino/Sorgenti/IRPixel/IRPixel.ino
@@ -31,14 +31,23 @@ void setup()
 }
 
 void loop() {
+  
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
     irrecv.resume(); // Receive the next value
 
     if( results.value == 0xFF22DD ) {
-        pixels.setPixelColor(0, pixels.Color(0,0,0)); // Moderately bright green color.
-        pixels.setPixelColor(1, pixels.Color(0,0,0)); // Moderately bright green color.
-        pixels.setPixelColor(2, pixels.Color(0,0,0)); // Moderately bright green color.
+        pixels.setPixelColor(0, pixels.Color(0,0,0)); // Spegne tutto
+        pixels.setPixelColor(1, pixels.Color(0,0,0));  
+        pixels.setPixelColor(2, pixels.Color(0,0,0)); 
+        pixels.show(); // This sends the updated pixel color to the hardware.
+    }
+
+    if( results.value == 0xFF52AD ) {
+        //                                   R   G   B
+        pixels.setPixelColor(0, pixels.Color(250,200,000)); // il più vicino ai fili rosso
+        pixels.setPixelColor(1, pixels.Color(250,200,000));  
+        pixels.setPixelColor(2, pixels.Color(250,200,000)); 
         pixels.show(); // This sends the updated pixel color to the hardware.
     }
 
@@ -50,19 +59,34 @@ void loop() {
     }
 
     if( results.value == 0xFF629D ) {
-        pixels.setPixelColor(0, pixels.Color(150,0,0)); // Moderately bright green color.
-        pixels.setPixelColor(1, pixels.Color(150,0,0)); // Moderately bright green color.
-        pixels.setPixelColor(2, pixels.Color(150,0,0)); // Moderately bright green color.
+        pixels.setPixelColor(0, pixels.Color(250,0,0)); // bright red color.
+        pixels.setPixelColor(1, pixels.Color(250,0,0)); 
+        pixels.setPixelColor(2, pixels.Color(250,0,0)); 
         pixels.show(); // This sends the updated pixel color to the hardware.
     }
 
-    if( results.value == 0xFFE21D ) {
-        pixels.setPixelColor(0, pixels.Color(0,0,150)); // Moderately bright green color.
-        pixels.setPixelColor(1, pixels.Color(0,0,150)); // Moderately bright green color.
-        pixels.setPixelColor(2, pixels.Color(0,0,150)); // Moderately bright green color.
-        pixels.show(); // This sends the updated pixel color to the hardware.
-    }
+    delay(1000);
   }
+
+  int valore = analogRead( A2 );
+  Serial.println( valore );
   
-  delay(100);
+      pixels.setPixelColor(0, pixels.Color(0,0,0)); 
+    pixels.setPixelColor(1, pixels.Color(0,0,0)); 
+    pixels.setPixelColor(2, pixels.Color(0,0,0)); 
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(valore);
+
+    pixels.setPixelColor(0, pixels.Color(250,0,0)); 
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(valore);
+
+    pixels.setPixelColor(1, pixels.Color(0,0,250)); 
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(valore);
+
+    pixels.setPixelColor(2, pixels.Color(0,250,0)); 
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(valore);
+
 }
